2b4cd46b5af770f8a92eb0d891075efd4aa41427
zxlzy1998 <1281926664@qq.com>
2021-10-19 18:22:08 +0800

    a

--- src/main/java/com/iic/graph/controller/MyController.java
+++ src/main/java/com/iic/graph/controller/MyController.java
@@ -92,7 +92,7 @@ public class MyController {
         student.setName(name);
         student.setAge(age);
 
-        System.out.println("branch1");
+        System.out.println("master");
         String json="";
         if(student !=null){
             ObjectMapper om = new ObjectMapper();
